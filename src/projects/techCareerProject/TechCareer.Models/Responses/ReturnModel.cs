812fbae854dc05ce21a903f57e76fc89c023d1de
imtalhaozer <imtalhaozer@gmail.com>
2024-11-29 14:18:29 +0300

    return model added

---
+++ src/projects/techCareerProject/TechCareer.Models/Responses/ReturnModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TechCareer.Models.Responses;
+
+public sealed class ReturnModel<T>
+{
+    public T Data { get; set; }
+    public bool Success { get; set; }
+
+    public string Message { get; set; }
+
+    public int StatusCode { get; set; }
+
+}
